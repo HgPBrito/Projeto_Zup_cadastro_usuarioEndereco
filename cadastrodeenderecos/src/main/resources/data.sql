940fea2ce5fb39e00b3ad60ca9c87c684c0076ec
Hugo Brito <hgbrito.tr@gmail.com>
2021-07-19 19:46:18 -0300

    implementacao das configuracoes do H2 e inclusao do data.sql para popular o banco ja com algumas informacoes

---
+++ cadastrodeenderecos/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+-- Insert de Usuarios
+INSERT INTO TB_USUARIO(nm_usuario, cd_cpf, dt_nasc, em_usuario) VALUES('HgPBrito', '23000379045', '1991-10-11', 'teste1@teste.com');
+
+INSERT INTO TB_USUARIO(nm_usuario, cd_cpf, dt_nasc, em_usuario) VALUES('Fernando', '18617334029', '1930-05-20', 'teste2@teste.com');
+
+INSERT INTO TB_USUARIO(nm_usuario, cd_cpf, dt_nasc, em_usuario) VALUES('Bruno', '02488910066', '1800-12-02', 'teste3@teste.com');
+
+INSERT INTO TB_USUARIO(nm_usuario, cd_cpf, dt_nasc, em_usuario) VALUES('Kathleen', '82352472083', '1995-03-22', 'teste4@teste.com');
+
+
+-- Insert de Enderecos
+INSERT INTO TB_ENDERECO(nm_estado, nm_cidade, cd_cep, nm_bairro, nm_logradouro, nm_complemento, cd_numero, cd_usuario) VALUES('PR', 'Almirante Tamandaré', '83506308', 'Bonfim', 'Rua Geovan Ribeiro', '', '42', '1');
+
+
